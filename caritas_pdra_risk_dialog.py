36214a1613687859a3701303d2968f2caf52aa7a
Ben Hur <benhur07b@users.noreply.github.com>
2018-03-19 10:02:28 +0800

    Edit Risk Computation GUI. Added functions to Risk Computation.

--- caritas_pdra_risk_dialog.py
+++ caritas_pdra_risk_dialog.py
@@ -21,15 +21,19 @@
  *                                                                         *
  ***************************************************************************/
 
+ Contains code for the PDRA Risk Computations.
 """
 
 __author__ = 'Ben Hur S. Pintor <bhs.pintor@gmail.com>'
 __date__ = '02/15/2018'
 
-import os
-
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from PyQt5 import QtWidgets
+from qgis.core import *
+
+import os
 
 # from .resources_rc import *
 from .caritas_pdra_risk_dialog_ui import Ui_CaritasPDRARiskDialog
@@ -37,7 +41,15 @@ from .caritas_pdra_risk_dialog_ui import Ui_CaritasPDRARiskDialog
 # FORM_CLASS, _ = uic.loadUiType(os.path.join(
 #     os.path.dirname(__file__), 'caritas_pdra_risk_dialog_ui.ui'))
 
-class CaritasPDRARiskDialog(QtWidgets.QDialog, Ui_CaritasPDRARiskDialog):
+# import the PDRA indicators
+from .caritas_pdra_utilities import *
+
+indicators_path = os.path.dirname(__file__) + '/indicators/pdra-household-indicators.csv'
+indicators = Indicators(indicators_path)
+
+
+class CaritasPDRARiskDialog(QDialog, Ui_CaritasPDRARiskDialog):
+
     def __init__(self, parent=None):
         """Constructor."""
         super(CaritasPDRARiskDialog, self).__init__(parent)
@@ -49,3 +61,66 @@ class CaritasPDRARiskDialog(QtWidgets.QDialog, Ui_CaritasPDRARiskDialog):
         self.parent = parent
         self.setupUi(self)
         self.setWindowTitle(self.tr('PDRA Risk Computations'))
+
+        # Setup button responses
+        self.buttonBox.accepted.connect(self.run)
+        self.hazSelectAllBtn.clicked.connect(self.hazListWidget.selectAll)
+        self.hazUnselectAllBtn.clicked.connect(self.hazListWidget.clearSelection)
+        self.vulSelectAllBtn.clicked.connect(self.vulListWidget.selectAll)
+        self.vulUnselectAllBtn.clicked.connect(self.vulListWidget.clearSelection)
+        self.capSelectAllBtn.clicked.connect(self.capListWidget.selectAll)
+        self.capUnselectAllBtn.clicked.connect(self.capListWidget.clearSelection)
+
+        # Populate the indicator widgets
+        self.catsListWidgets = [self.capListWidget, self.vulListWidget, self.capListWidget]
+        self.indicator_names_per_category = indicators.get_dict_of_indicator_names_per_category()
+        self.hazListWidget.addItems(sorted(self.indicator_names_per_category['Hazard']))
+        self.vulListWidget.addItems(sorted(self.indicator_names_per_category['Vulnerability']))
+        self.capListWidget.addItems(sorted(self.indicator_names_per_category['Capacity']))
+
+
+    def get_selected_indicator_codes(self):
+
+        selected_haz = self.hazListWidget.selectedIndexes()
+        selected_haz_names = [str(x.data()) for x in selected_exp]
+        selected_haz_codes = [indicators.get_indicator_code_from_name(name) for name in selected_haz_names]
+
+        selected_vul = self.vulListWidget.selectedIndexes()
+        selected_vul_names = [str(x.data()) for x in selected_exp]
+        selected_vul_codes = [indicators.get_indicator_code_from_name(name) for name in selected_vul_names]
+
+        selected_cap = self.capListWidget.selectedIndexes()
+        selected_cap_names = [str(x.data()) for x in selected_exp]
+        selected_cap_codes = [indicators.get_indicator_code_from_name(name) for name in selected_cap_names]
+
+        selected_all_codes = selected_haz_codes + selected_vul_codes + selected_cap_codes
+
+        return {
+            "Hazard": selected_haz_codes,
+            "Vulnerability": selected_vul_codes,
+            "Capacity": selected_cap_codes,
+            "All": selected_all_codes
+        }
+
+
+    def count_main_indicators(self):
+
+        selected_indicators = self.get_selected_indicator_codes()
+        count_haz = len(set(["_".join(i.split("_")[:2]) for i in selected_indicators["Hazard"]]))
+        count_vul = len(set(["_".join(i.split("_")[:2]) for i in selected_indicators["Vulnerability"]]))
+        count_cap = len(set(["_".join(i.split("_")[:2]) for i in selected_indicators["Hazard"]]))
+
+        return {
+            "Hazard": count_haz,
+            "Vulnerability": count_vul,
+            "Capacity": count_cap,
+            "All": count_haz + count_vul - count_cap  # computation or risk is H + V - C
+        }
+
+
+    def run(self):
+        pass
+
+
+    def compute_risk(self):
+        pass
--- caritas_pdra_utilities.py
+++ caritas_pdra_utilities.py
@@ -21,4 +21,132 @@
  *                                                                         *
  ***************************************************************************/
 
+ Contains utility functions.
 """
+
+__author__ = 'Ben Hur S. Pintor <bhs.pintor@gmail.com>'
+__date__ = '02/15/2018'
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+from qgis.core import *
+import os
+import csv
+import re
+
+
+class Indicators():
+    """Class for the PDRA Indicators.
+
+    Format of Household PDRA Indicators CSV is:
+    index   value
+    0       INDICATOR_CODE
+    1       INDICATOR_NAME
+    2       CATEGORY
+    3       RISK_VALUE
+    """
+
+    def __init__(self, indicators_path):
+        self.indicators_path = indicators_path
+        self.indicators_list = self.get_indicators_list()
+
+
+    def get_indicators_list(self):
+        """Reads the indicators CSV provided by the indicators path and saves
+        it to a list.
+        """
+
+        with open(self.indicators_path, 'r') as f:
+            reader = csv.reader(f)
+            return list(reader)[1:]  # exclude headers
+
+
+    def get_indicator_codes(self):
+
+        return [i[0] for i in self.indicators_list]
+
+
+    def get_indicator_codes_from_indicators_list(self, indicators_list):
+
+        return [i[0] for i in indicators_list]
+
+
+    def get_indicator_names(self):
+
+        return [i[1] for i in self.indicators_list]
+
+
+    def get_indicator_names_from_indicators_list(self, indicators_list):
+
+        return [i[1] for i in indicators_list]
+
+
+    def get_indicator_name_from_code(self, indicator_code):
+        """Returns the INDICATOR_NAME of the given INDICATOR_CODE"""
+
+        if indicator_code in self.get_indicator_codes():  # if indicator_code exists, return the indicator_name
+            return [i[1] for i in self.indicators_list if i[0] == indicator_code][0]
+        else:
+            return None
+
+
+    def get_indicator_code_from_name(self, indicator_name):
+        """Returns the INDICATOR_CODE of the given INDICATOR_NAME"""
+
+        if indicator_name in self.get_indicator_names():  # if indicator_name exists, return the indicator_code
+            return [i[0] for i in self.indicators_list if i[1] == indicator_name][0]
+        else:
+            return None
+
+
+    def get_list_of_categories(self):
+        """Returns the list of categories"""
+
+        return sorted(list(set([i[2].capitalize() for i in self.indicators_list])))  # capitalize is called so that 'hazard' and 'Hazard' are considere the same
+
+
+    def get_list_of_indicators_in_category(self, category):
+        """Returns a list of indicators for a given category"""
+
+        if category.capitalize() in self.get_list_of_categories():
+            return [i for i in self.indicators_list if i[2].capitalize() == category.capitalize()]
+
+        else:
+            return None
+
+
+    def get_dict_of_indicators_per_category(self):
+        """Returns a dict grouping the indicators per category"""
+
+        cats_dict = {}
+        categories = self.get_list_of_categories()
+
+        for category in categories:
+            cats_dict[category] = self.get_list_of_indicators_in_category(category)
+
+        return cats_dict
+
+
+    def get_dict_of_indicator_names_per_category(self):
+        """Returns a dict grouping the indicator names per category"""
+
+        names_dict = {}
+        categories = self.get_list_of_categories()
+
+        for category in categories:
+            names_dict[category] = [i[1] for i in self.get_list_of_indicators_in_category(category)]
+
+        return names_dict
+
+
+    def get_dict_of_indicator_codes_per_category(self):
+        """Returns a dict grouping the indicator names per category"""
+
+        codes_dict = {}
+        categories = self.get_list_of_categories()
+
+        for category in categories:
+            codes_dict[category] = [i[0] for i in self.get_list_of_indicators_in_category(category)]
+
+        return codes_dict
